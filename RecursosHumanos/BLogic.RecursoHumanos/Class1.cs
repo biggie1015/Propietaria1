ac5e092ece2d57739afa7b9d7b66ca35e5678637
biggie1015 <ismel.1015@gmail.com>
2020-10-08 20:50:04 -0400

    Revert "Gestion idiomas"

--- RecursosHumanos/BLogic.RecursoHumanos/Class1.cs
+++ RecursosHumanos/BLogic.RecursoHumanos/Class1.cs
@@ -1,16 +1,8 @@
-﻿using DataAccess.RecursosHumanos;
-using DataAccess.RecursosHumanos.BaseRepository;
-using System;
+﻿using System;
 
 namespace BLogic.RecursoHumanos
 {
     public class Class1
     {
-        Class2 clas = new Class2();
-        GestionCompetencias competencias = new GestionCompetencias();
-        public void resaltar()
-        {
-            competencias.GetCompetencias();
-        }
     }
 }
--- RecursosHumanos/RecursosHumanos/FrmCompetencias/FrmComptenecia.cs
+++ RecursosHumanos/RecursosHumanos/FrmCompetencias/FrmComptenecia.cs
@@ -7,26 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using BusnnessLogic.RecursosHumanos;
 
 namespace RecursosHumanos.FrmCompetencias
 {
     public partial class FrmComptenecia : Form
     {
-        private GCompetenciasLogic competencias;
-        private  bool Editar = false;
         public FrmComptenecia()
         {
             InitializeComponent();
-            competencias = new GCompetenciasLogic();
             
         }
 
         private void FrmComptenecia_Load(object sender, EventArgs e)
         {
-              dgvCompetencias.DataSource = competencias.GetAll();
-              dgvCompetencias.ClearSelection();
-         
+            Competencia competencia = new Competencia();
+            competencia.ID = 15;
+            competencia.Descripcion = "Desarrollo Web";
+            dgvCompetencias.Columns.Add("ID","ID");
+            dgvCompetencias.Columns.Add("Descripcion", "Descripcion");
+            dgvCompetencias.Rows.Add("32","Desarrollo Web");
+            dgvCompetencias.Rows.Add("14", "Desarrollo Angular");
             //lblCompetencias.Left = (lblCompetencias.Parent.Width - lblCompetencias.Width) / 2;
             //lblCompetencias.Top = (lblCompetencias.Parent.Height - lblCompetencias.Height) / 2;
 
@@ -36,75 +36,11 @@ namespace RecursosHumanos.FrmCompetencias
         {
            
         }
-
-        private void btnEditar_Click(object sender, EventArgs e)
-        {
-            if(dgvCompetencias.SelectedRows.Count > 0)
-            {
-                Editar = true;
-                txtID.Text = dgvCompetencias.CurrentRow.Cells[0].Value.ToString();
-                txtDescripcion.Text = dgvCompetencias.CurrentRow.Cells[1].Value.ToString();
-         
-                
-            }
-            else
-            {
-                MessageBox.Show("Debe elegir una fila");
-            }
-        }
-
-        private void btnGuardar_Click(object sender, EventArgs e)
-        {
-            if (Editar)
-            {
-                
-                competencias.Update(txtDescripcion.Text, Convert.ToInt32(txtID.Text));
-                txtID.Text = "";
-                txtDescripcion.Text = "";
-            }
-            else
-            {
-               
-               
-                competencias.Create(txtDescripcion.Text);
-                txtID.Text = "";
-                txtDescripcion.Text = "";
-                
-            }
-            Editar = false;
-            dgvCompetencias.DataSource = competencias.GetAll();
-        }
-
-        private void btnEliminar_Click(object sender, EventArgs e)
-        {
-            if (dgvCompetencias.SelectedRows.Count > 0)
-            {
-              
-                txtID.Text = dgvCompetencias.CurrentRow.Cells[0].Value.ToString();
-                competencias.Delete(Convert.ToInt32(txtID.Text));
-                dgvCompetencias.DataSource = competencias.GetAll();
-            }
-            else
-            {
-                MessageBox.Show("Debe elegir una fila");
-            }
-
-        }
-
-        private void txtSearch_TextChanged(object sender, EventArgs e)
-        {
-
-            dgvCompetencias.DataSource = competencias.getByID(txtSearch.Text);
-        }
-
-        private void btnNuevo_Click(object sender, EventArgs e)
-        {
-            txtID.Text = "";
-            txtDescripcion.Text = "";
-            Editar = false;
-
-        }
     }
 
-    
+    class Competencia
+    {
+        public int ID { get; set; }
+        public string Descripcion { get; set; }
+    }
 }
